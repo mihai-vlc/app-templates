ab0b960989016f564c7e89eafdb61cb6b0efc145
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2023-05-21 16:00:33 +0300

    Adds support for loading environment variables

--- python/src/my_app/main.py
+++ python/src/my_app/main.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from my_app.calculator import add
 
+load_dotenv()
+
 if __name__ == "__main__":
-    print("Calculator app")
+    print(f"Calculator app with log level `{os.getenv('APP_LOG_LEVEL')}`")
     a = int(input("a = "))
     b = int(input("b = "))
     print("a + b =", add(a, b))
---
+++ python/src/my_app/test_main.py
@@ -0,0 +1,6 @@
+
+import os
+
+def test_has_env_variables():
+    assert os.getenv("APP_LOG_LEVEL") is not None
+
