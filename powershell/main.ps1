4761320fa0df339bad38fba3cd7443195f629bb0
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2023-11-20 17:04:19 +0200

    Adds support for environment variables loaded from .env file

--- powershell/main.ps1
+++ powershell/main.ps1
@@ -1,7 +1,22 @@
+#requires -Module @{ModuleName='dotenv'; ModuleVersion="0.4.0"}
+# To get the most recent version
+# TODO: Look into PSDepend as an alternative
+# Install-ModuleFromGitHub -GitHubRepo stopthatastronaut/poshdotenv -moduleName dotenv -Scope CurrentUser
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+
+
+Import-Module dotenv -Function Set-DotEnv, Remove-DotEnv
+Set-DotEnv
+
 Import-Module ./Math.psm1 -Force -Function Invoke-Add, Invoke-Subtract, New-Calculator
+
 
 function Invoke-Cleanup() {
     Remove-Module Math
+    Remove-DotEnv
 }
 
 function Invoke-Main() {
@@ -15,6 +30,8 @@ function Invoke-Main() {
     $c1.Add(8)
 
     Write-Output "Calculator value = $($c1.Value)"
+
+    Write-Output "$($env:APP_THEME)"
 }
 
 
